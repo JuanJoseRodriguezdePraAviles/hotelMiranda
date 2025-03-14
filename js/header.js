0a471cff8e4e3b69f30a500ca1dc78809017aac5
JuanJoseRodriguezdePraAviles <45094901+JuanJoseRodriguezdePraAviles@users.noreply.github.com>
2025-03-14 16:53:56 +0100

    Header css and js implemented for mobile

---
+++ js/header.js
@@ -0,0 +1,19 @@
+let menuState = false;
+
+const menuIcon = document.querySelector('img.hamburguer');
+const menuLinkContainer = document.querySelector('div.menu-extended');
+
+menuIcon.addEventListener('click', () => {
+    if(!menuState){
+        menuState = true;
+        menuIcon.setAttribute('src', './../Images/close-menu.svg');
+        menuIcon.setAttribute("class", "close-icon");
+        menuLinkContainer.style.display="inline-block";
+    } else {
+        menuState = false;
+        menuIcon.setAttribute('src', './../Images/hamburguer.svg');
+        menuIcon.setAttribute("class", "hamburguer");
+        menuLinkContainer.style.display="none";
+    }
+});
+
