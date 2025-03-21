a751aff37514bfa9b71ad19d0414f87d8036ca8d
JuanJoseRodriguezdePraAviles <45094901+JuanJoseRodriguezdePraAviles@users.noreply.github.com>
2025-03-21 11:44:17 +0100

    relative path for deploy

--- js/header.js
+++ js/header.js
@@ -6,12 +6,12 @@ const menuLinkContainer = document.querySelector('div.menu-extended');
 menuIcon.addEventListener('click', () => {
     if(!menuState){
         menuState = true;
-        menuIcon.setAttribute('src', '/hotelMiranda/../Images/close-menu.svg');
+        menuIcon.setAttribute('src', '/hotelMiranda/Images/close-menu.svg');
         menuIcon.setAttribute("class", "close-icon");
         menuLinkContainer.style.display="inline-block";
     } else {
         menuState = false;
-        menuIcon.setAttribute('src', '/hotelMiranda/../Images/hamburguer.svg');
+        menuIcon.setAttribute('src', '/hotelMiranda/Images/hamburguer.svg');
         menuIcon.setAttribute("class", "hamburguer");
         menuLinkContainer.style.display="none";
     }
