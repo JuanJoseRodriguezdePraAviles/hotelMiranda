88806b367fd25aea6c60c20fa3ff87bab014e3aa
root <root@DESKTOP-IF4JS77.localdomain>
2025-07-31 13:24:39 +0200

    fixed display of rooms

--- resources/views/rooms/list.blade.php
+++ resources/views/rooms/list.blade.php
@@ -16,32 +16,34 @@
 </div>
 <div class="swiper mySwiper sliderWrapper">
     <div class="rooms-container swiper-wrapper">
-        @foreach($rooms->chunk(4) as $roomGroup)
+        @foreach($rooms->chunk(12) as $roomGroup)
         <div class="swiper-slide">
-            @foreach($roomGroup as $room)
-            <div class="room-card">
-                <div class="image-container">
-                    <img src="{{ json_decode($room->photos)[0] }}" />
-                    <div class="room-features">
-                        <img src="./Images/icon1.svg" />
-                        <img src="./../Images/icon2.svg" />
-                        <img src="./../Images/icon3.svg" />
-                        <img src="./../Images/icon4.svg" />
-                        <img src="./../Images/icon5.svg" />
-                        <img src="./../Images/icon6.svg" />
-                        <img src="./../Images/icon7.svg" />
+            <div class="room-grid">
+                @foreach($roomGroup as $room)
+                <div class="room-card">
+                    <div class="image-container">
+                        <img src="{{ json_decode($room->photos)[0] }}" />
+                        <div class="room-features">
+                            <img src="./Images/icon1.svg" />
+                            <img src="./../Images/icon2.svg" />
+                            <img src="./../Images/icon3.svg" />
+                            <img src="./../Images/icon4.svg" />
+                            <img src="./../Images/icon5.svg" />
+                            <img src="./../Images/icon6.svg" />
+                            <img src="./../Images/icon7.svg" />
+                        </div>
+                    </div>
+                    <div class="image-description">
+                        <h2>{{ $room->roomName }}</h2>
+                        <p>{{ $room->description }}</p>
+                        <div>
+                            <p class="room-price">{{ $room->price }}/Night</p>
+                            <p class="room-status"><a href="{{ url('/' . $room->id) }}">Booking Now</a></p>
+                        </div>
                     </div>
                 </div>
-                <div class="image-description">
-                    <h2>{{ $room->roomName }}</h2>
-                    <p>{{ $room->description }}</p>
-                    <div>
-                        <p class="room-price">{{ $room->price }}/Night</p>
-                        <p class="room-status"><a href="{{ url('/' . $room->id) }}">Booking Now</a></p>
-                    </div>
-                </div>
+                @endforeach
             </div>
-            @endforeach
         </div>
         @endforeach
     </div>
@@ -54,7 +56,6 @@
     window.onload = function () {
         const swiper = new Swiper(".mySwiper", {
             slidesPerView: 1,
-            centeredSlides: true,
             spaceBetween: 30,
             pagination: {
                 el: ".swiper-pagination",
@@ -69,7 +70,7 @@
             },
             breakpoints: {
                 1000: {
-                    slidesPerView: 2.5
+                    slidesPerView: 1
                 }
             }
         });
